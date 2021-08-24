af3e84f2826db0082430b912939b325a93b2fcc9
Mosuhyeok <msv31v@naver.com>
2021-08-24 15:28:08 +0900

    upload

---
+++ src/main/java/hello/springmvc/basic/requestmapping/ResponseBodyController.java
@@ -0,0 +1,56 @@
+package hello.springmvc.basic.requestmapping;
+
+import hello.springmvc.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@Slf4j
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2(HttpServletResponse response)  {
+        return new ResponseEntity<String>("ok", HttpStatus.OK);
+    }
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3()  {
+        return "ok";
+    }
+
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1(){
+        HelloData helloData = new HelloData();
+        helloData.setUsername("usrA");
+        helloData.setAge(10);
+
+        return new ResponseEntity<>(helloData,HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2(){
+        HelloData helloData = new HelloData();
+        helloData.setUsername("usrA");
+        helloData.setAge(10);
+
+        return helloData;
+    }
+
+
+
+
+
+
+}
